c8a040db0d65e9147e73490eec6167b6b37b1ea4
Wenhao Guo <gwh1742072653@gmail.com>
2020-03-30 17:06:24 +0000

    Done.

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -6,6 +6,13 @@
 # suite_e: "Crumpet the Elf"
 # }
 
+winner = ""
 def select_winner(passengers)
-  # add the code snippet here!
+  passengers.each do |suite, name|
+  if suite == :suite_a && name.start_with?("A")
+    winner = name
+  end
+  return winner
 end
+end
+
