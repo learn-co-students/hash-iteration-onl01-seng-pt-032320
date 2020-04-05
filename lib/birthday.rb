9e18069088a7343f681516fbfc096f2e773187b4
Daniel Hoyte <daniel.hoyte59@gmail.com>
2020-04-05 17:19:30 +0000

    Done.

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -10,4 +10,13 @@ birthday_kids.each do |kids_name, age|
 end
 end
 
+ def age_appropriate_birthday(birthday_kids)
+  birthday_kids.each do |kid, age|
+    if age < 12
+      puts  "Happy Birthday #{kid}! You are now #{age} years old!"
+  else puts"You are too old for this."
+   end
+ end
+ end
+
 
